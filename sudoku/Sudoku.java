c4e0d40804b13946fd348028dbef437af46761f4
Colby Austin Brown <colbyaustinbrown@gmail.com>
2024-07-08 19:43:40 -0700

    Sudoku solution -- not fast enough :(

--- sudoku/Sudoku.java
+++ sudoku/Sudoku.java
@@ -3,7 +3,7 @@ import java.util.*;
 
 class Sudoku {
     public static void main(String[] args) throws Exception {
-        Scanner input = new Scanner(new File("sample.in"));
+        Scanner input = new Scanner(System.in);
 
         int[][] board = new int[9][9];
         for (int y = 0; y < 9; y++) {
@@ -14,9 +14,36 @@ class Sudoku {
                 }
             }
         }
+
+        if (find_solution(board, 0, 0)) {
+            for (int y = 0; y < 9; y++) {
+                for (int x = 0; x < 9; x++) {
+                    System.out.print(board[y][x]);
+                }
+                System.out.println();
+            }
+        } else {
+            System.out.println("No Solution");
+        }
     }
 
-    public boolean is_valid(int[][] board) {
+    // Finds the solution in place -- mutating the board parameter passed in
+    // on return TRUE, board contains a solution
+    // on FALSE, board is the same as what was passed in
+    static public boolean find_solution(int[][] board, int y, int x) {
+        if (x == 9) return find_solution(board, y + 1, 0);
+        if (y == 9) return is_valid(board);
+        if (board[y][x] != 0) return find_solution(board, y, x + 1);
+
+        for (int i = 1; i <= 9; i++) {
+            board[y][x] = i;
+            if (find_solution(board, y, x + 1)) return true;
+        }
+        board[y][x] = 0;
+        return false;
+    }
+
+    static public boolean is_valid(int[][] board) {
         // checks the rows and the columns
         for (int i = 0; i < 9; i++) {
             boolean[] used_rows = new boolean[9];
@@ -34,8 +61,8 @@ class Sudoku {
             for (int x = 0; x < 9; x++) {
                 int block_row = y / 3;
                 int block_col = x / 3;
-                if (used[block_row][block_col][board[y][x]]) return false;
-                used[block_row][block_col][board[y][x]] = true;
+                if (used[block_row][block_col][board[y][x] - 1]) return false;
+                used[block_row][block_col][board[y][x] - 1] = true;
             }
         }
 
